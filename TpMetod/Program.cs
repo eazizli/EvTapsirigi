d2e4f2b2777eb16f7a44120e494382a6952bb608
Elvin Ezizli <elvin@example.com>
2023-03-29 19:48:14 +0400

    .

---
+++ TpMetod/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine(Sade(11));
+        //Console.WriteLine(Yeni(20, 23));
+        //Console.WriteLine("Hello, World!");
+
+    }
+        //static int Yeni(int a,int b) {
+        //    int cem = 0;
+        //    if (a > b)
+        //    {
+
+        //        Console.WriteLine("sistem iwlemez");
+
+        //    }
+        //    for (int i = a; i < b; i++)
+        //    {
+
+
+        //        if (i % 7 == 0 || i % 3 == 0)
+        //        {
+        //            cem += i;
+
+
+
+        //        }
+
+        //    }
+
+
+        //return cem;
+        //}
+    //    static bool Sade(int a)
+    //{
+       
+        
+    //    if (a == 1)
+    //    {
+    //        Console.WriteLine("eded ne sadedir ne murekkebdir");
+    //    }
+    //    for (int i = 2; i < a; i++)
+    //    {
+    //        if (a % i == 0)
+    //        {
+
+
+    //            Console.WriteLine("murekkebdir");
+    //            return false;
+                
+    //            break;
+
+    //        }
+    //    }
+    // return true;
+    //    {
+    //        Console.WriteLine(("bu eded sadedir")); 
+    //    }
+    //}
+
+    }
